a6bba3e0d5bc38024d881427b6cb00d794af00d4
akhilkhemani <akhilkhemani@Akhils-MBP.attlocal.net>
2015-05-17 12:21:20 -0700

    First Check In

---
+++ Serve/ReviewSubmitViewController.m
@@ -0,0 +1,289 @@
+//
+//  ReviewSubmitViewController.m
+//  Serve
+//
+//  Created by Akhil Khemani on 5/14/15.
+//  Copyright (c) 2015 Akhil Khemani. All rights reserved.
+//
+
+#import "ReviewSubmitViewController.h"
+
+const CGFloat reviewProgressButtonSize = 19.0f;
+const CGFloat reviewProgressButtonY = 365.0f;
+const CGFloat reviewProgressButtonX = 80.0f;
+const CGFloat reviewProgressButtonInset = -2.0f;
+const CGFloat reviewProgressIndicatorTextSize = 9.0f;
+
+static NSArray *deleteButtonActionSheetItems = nil;
+const CGFloat reviewDeleteButtonTag = 1;
+
+@interface ReviewSubmitViewController ()
+
+@property (nonatomic, strong) UIView *progressIndicator;
+@property (nonatomic, strong) UIActionSheet *deleteButtonActionSheet;
+
+- (IBAction)submitButtonPressed:(id)sender;
+- (IBAction)backButtonPressed:(id)sender;
+- (void)showActionSheet:(id)sender;
+
+@end
+
+@implementation ReviewSubmitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    deleteButtonActionSheetItems = [[NSArray alloc] initWithObjects:@"Are you sure you want to delete the listing?",
+                                    @"Discard Listing",@"Cancel", nil];
+    
+    [self setUpActionSheets];
+
+    [self.navigationItem setTitle:@"Review & Submit"];
+     self.navigationItem.hidesBackButton = YES;
+    
+
+    
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    
+    //int i = 1;
+    self.navigationController.toolbarHidden = NO;
+    
+    UIBarButtonItem *itemSpace = [[UIBarButtonItem alloc]
+                                  initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                  target:nil
+                                  action:nil];
+    
+    
+//    UIBarButtonItem *removeViews = [[UIBarButtonItem alloc]
+//                                    initWithTitle:@"Close Views"
+//                                    style: UIBarButtonItemStyleBordered
+//                                    target:self
+//                                    action:@selector(checkButtonClick:)];
+//    removeViews.tag = i++;
+    
+
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Back"
+                                   style: UIBarButtonItemStylePlain
+                                   target:self
+                                   action:@selector(backButtonPressed:)];
+
+    
+    UIBarButtonItem *submitButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Submit"
+                                   style: UIBarButtonItemStylePlain
+                                   target:self
+                                   action:@selector(submitButtonPressed:)];
+    
+    //trash button
+    UIImage *trashImage = [UIImage imageNamed:@"trash.png"];
+    UIButton *button =  [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setImage:trashImage forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(showActionSheet:) forControlEvents:UIControlEventTouchUpInside];
+    [button setFrame:CGRectMake(0, 0, trashImage.size.width, trashImage.size.height)];
+    button.tag = reviewDeleteButtonTag;
+//    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(3, 21, 50, 20)];
+//    [label setText:@"Discard"];
+//    [label setFont:[UIFont systemFontOfSize:9.0f]];
+//    label.textAlignment = NSTextAlignmentCenter;
+//    [label setTextColor:[UIColor blackColor]];
+//    [label setBackgroundColor:[UIColor clearColor]];
+    //[button addSubview:label];
+    UIBarButtonItem *trashButton = [[UIBarButtonItem alloc] initWithCustomView:button];
+    /////
+    
+    ///setting color of back and continue buttons to black
+    [submitButton setTitleTextAttributes:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      [UIColor blackColor], NSForegroundColorAttributeName,[UIFont fontWithName:@"Helvetica-Bold" size:12.0],
+      NSFontAttributeName, nil]forState:UIControlStateNormal];
+
+    [backButton setTitleTextAttributes:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      [UIColor blackColor], NSForegroundColorAttributeName,[UIFont fontWithName:@"Helvetica-Bold" size:12.0],
+      NSFontAttributeName, nil]forState:UIControlStateNormal];
+    ////////////////////////////////////////////////////////////
+    
+    
+    NSArray *items = [NSArray arrayWithObjects:backButton, itemSpace, trashButton, itemSpace, submitButton, nil];
+    self.toolbarItems = items;
+    
+    [self.view addSubview:self.progressIndicator];
+}
+
+- (UIView *)progressIndicator {
+    
+    _progressIndicator = [[UIView alloc]initWithFrame:CGRectMake(35, 45, 100, 40)];
+    
+    UIButton *step1Button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [step1Button setFrame:CGRectMake(_progressIndicator.frame.origin.x+16,_progressIndicator.frame.origin.y,reviewProgressButtonSize, reviewProgressButtonSize)];
+    [step1Button setTitle:@"1" forState:UIControlStateNormal];
+    [step1Button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    //[step1Button addTarget:self action:@selector(toggleItemType:) forControlEvents:UIControlEventTouchUpInside];
+    step1Button.layer.borderWidth=1.0f;
+    step1Button.layer.borderColor=[[UIColor blackColor] CGColor];
+    step1Button.layer.backgroundColor = [[UIColor blackColor] CGColor];
+    step1Button.layer.cornerRadius = 10;
+    step1Button.contentEdgeInsets = UIEdgeInsetsMake(reviewProgressButtonInset, 0.0, 0.0, 0.0);
+    
+    UILabel *step1Label = [[UILabel alloc]initWithFrame:CGRectMake(_progressIndicator.frame.origin.x, _progressIndicator.frame.origin.y+reviewProgressButtonSize, 60, 20)];
+    [step1Label setText:@"Item Details"];
+    [step1Label setFont:[UIFont systemFontOfSize:reviewProgressIndicatorTextSize]];
+    
+    UIView *lineView1 = [[UIView alloc] initWithFrame:CGRectMake(_progressIndicator.frame.origin.x+16+reviewProgressButtonSize, _progressIndicator.frame.origin.y+reviewProgressButtonSize/2, 75, 1.0f)];
+    lineView1.backgroundColor = [UIColor blackColor];
+    
+    UIButton *step2Button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [step2Button setFrame:CGRectMake(_progressIndicator.frame.origin.x+16+reviewProgressButtonSize+lineView1.frame.size.width,_progressIndicator.frame.origin.y,reviewProgressButtonSize, reviewProgressButtonSize)];
+    [step2Button setTitle:@"2" forState:UIControlStateNormal];
+    [step2Button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    //[part1Button addTarget:self action:@selector(toggleItemType:) forControlEvents:UIControlEventTouchUpInside];
+    step2Button.layer.borderWidth=1.0f;
+    step2Button.layer.borderColor=[[UIColor blackColor] CGColor];
+    step2Button.layer.backgroundColor = [[UIColor blackColor] CGColor];
+    step2Button.layer.cornerRadius = 10;
+    step2Button.contentEdgeInsets = UIEdgeInsetsMake(reviewProgressButtonInset, 0.0, 0.0, 0.0);
+    
+    UILabel *step2Label = [[UILabel alloc]initWithFrame:CGRectMake(step1Label.frame.origin.x+lineView1.frame.size.width+8, _progressIndicator.frame.origin.y+reviewProgressButtonSize, 80, 20)];
+    [step2Label setText:@"Pickup Information"];
+    [step2Label setFont:[UIFont systemFontOfSize:reviewProgressIndicatorTextSize]];
+   
+    
+    UIView *lineView2 = [[UIView alloc] initWithFrame:CGRectMake(lineView1.frame.origin.x+lineView1.frame.size.width+reviewProgressButtonSize, _progressIndicator.frame.origin.y+reviewProgressButtonSize/2, 75, 2.0f)];
+    lineView2.backgroundColor = [UIColor blackColor];
+    
+    
+    UIButton *step3Button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [step3Button setFrame:CGRectMake(lineView2.frame.origin.x+lineView2.frame.size.width,_progressIndicator.frame.origin.y,reviewProgressButtonSize, reviewProgressButtonSize)];
+    [step3Button setTitle:@"3" forState:UIControlStateNormal];
+    [step3Button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    //[step3Button addTarget:self action:@selector(toggleItemType:) forControlEvents:UIControlEventTouchUpInside];
+    step3Button.layer.borderWidth=1.0f;
+    step3Button.layer.borderColor=[[UIColor blackColor] CGColor];
+    step3Button.layer.backgroundColor = [[UIColor blackColor] CGColor];
+    step3Button.layer.cornerRadius = 10;
+    step3Button.contentEdgeInsets = UIEdgeInsetsMake(reviewProgressButtonInset, 0.0, 0.0, 0.0);
+    
+    UILabel *step3Label = [[UILabel alloc]initWithFrame:CGRectMake(step2Label.frame.origin.x+lineView2.frame.size.width+26, _progressIndicator.frame.origin.y+reviewProgressButtonSize, 80, 20)];
+    [step3Label setText:@"Review/Submit"];
+    [step3Label setFont:[UIFont systemFontOfSize:reviewProgressIndicatorTextSize]];
+    [step3Label setTextColor:[UIColor redColor]];
+    
+    [_progressIndicator addSubview:step1Button];
+    [_progressIndicator addSubview:step1Label];
+    [_progressIndicator addSubview:lineView1];
+    [_progressIndicator addSubview:step2Button];
+    [_progressIndicator addSubview:step2Label];
+    [_progressIndicator addSubview:lineView2];
+    [_progressIndicator addSubview:step3Button];
+    [_progressIndicator addSubview:step3Label];
+    
+    return _progressIndicator;
+}
+
+- (void) checkButtonClick:(id)sender {
+    
+    //get reference to the button that requested the action
+    UIBarButtonItem *myButton = (UIBarButtonItem *)sender;
+    NSLog(@"Clicked on one of the toolbar buttons");
+    
+    //check which button it is, if you have more than one button on the screen
+    //you must check before taking necessary action
+    switch (myButton.tag) {
+        case 1:
+            //hide the navigation bar
+            [self.navigationController setNavigationBarHidden:YES animated:YES];
+            break;
+            
+        case 2:
+            
+        {
+            //Get the current array of View Controllers
+            NSArray *currentControllers = self.navigationController.viewControllers;
+            
+            //Create a mutable array out of this array
+            NSMutableArray *newControllers = [NSMutableArray arrayWithArray:currentControllers];
+            
+            //create an array set of interger indexes
+            NSMutableIndexSet *indexes = [NSMutableIndexSet
+                                          indexSetWithIndexesInRange:NSMakeRange(1, 2)];
+            
+            //Remove the objects from the controller array based on the indexes
+            [newControllers removeObjectsAtIndexes:indexes];
+            
+            //Assign the manipulated array to the Navigation Controller with animation
+            [self.navigationController setViewControllers:newControllers
+                                                 animated:YES];
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
+}
+
+- (IBAction)backButtonPressed:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)submitButtonPressed:(id)sender
+{
+    NSLog(@"Trying to submit");
+}
+
+
+- (void) setUpActionSheets{
+    
+    self.deleteButtonActionSheet= [[UIActionSheet alloc]initWithTitle:[deleteButtonActionSheetItems objectAtIndex:0] delegate:self cancelButtonTitle:[deleteButtonActionSheetItems objectAtIndex:2] destructiveButtonTitle:[deleteButtonActionSheetItems objectAtIndex:1] otherButtonTitles:nil, nil];
+    
+}
+
+- (void) showActionSheet:(id)sender {
+    NSInteger senderTag = [sender tag];
+    
+    if(senderTag == reviewDeleteButtonTag)
+    {
+        [self.deleteButtonActionSheet showInView:self.view];
+    }
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+    
+    if  ([buttonTitle isEqualToString:@"Discard Listing"])
+    {
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    }
+    
+    if ([buttonTitle isEqualToString:@"Other Button 1"]) {
+        NSLog(@"Other 1 pressed");
+    }
+    if ([buttonTitle isEqualToString:@"Other Button 2"]) {
+        NSLog(@"Other 2 pressed");
+    }
+    if ([buttonTitle isEqualToString:@"Other Button 3"]) {
+        NSLog(@"Other 3 pressed");
+    }
+    if ([buttonTitle isEqualToString:@"Cancel Button"]) {
+        NSLog(@"Cancel pressed --> Cancel ActionSheet");
+    }
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Serve/SharedUtils.m
@@ -0,0 +1,13 @@
+//
+//  SharedUtils.m
+//  Serve
+//
+//  Created by Akhil Khemani on 5/16/15.
+//  Copyright (c) 2015 Akhil Khemani. All rights reserved.
+//
+
+#import "SharedUtils.h"
+
+@implementation SharedUtils
+
+@end
